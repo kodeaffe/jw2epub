440acdfbcea31b7ddc7be372f841eaba2be22e14
kodeaffe <git@kodeaffe.de>
2017-04-02 13:14:42 +0200

    Updated project to support new JW website

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-README
-======
+# Jungle World 2 EPUB
 
-Alas, there is no epub version of the Jungle World, https://jungle-world.com .
+There is currently no official epub version of the [Jungle World](https://jungle.world).
 
 Hence this little Python3 script to download the current issue and its cover
 image and then convert it to epub.
 
+### NOTE
 
-Install
-=======
+This version supports only the new version of the website as of March 2017.
+The old version is supported up to git tag `pre201703`.
+
+
+
+## Install
 
 	$ virtualenv --python=python3 venv
 	$ source venv/bin/activate
@@ -17,27 +21,16 @@ Install
 	$ pip install -r requirements.txt
 
 
-Documentation
-=============
 
-If you want to generate documentation, you need to install sphinx and run make:
-
-	$ pip install Sphinx
-	$ cd docs/ && make html
-
-Entry point for the generated documentation is at `docs/_build/html/index.html`
-
-
-Usage
-=====
+## Usage
 
 This will download and convert the current issue:
 
-	$ ./jw2epub
+	$ ./jw2epub.py
 
 If you want to download a specific issue, use this invocation:
 
-	$ ./jw2epub 2017.05
+	$ ./jw2epub.py 2017/12
 
 The generated epub file is put into the current directory.
 
@@ -47,5 +40,16 @@ the configuration defaults, copy settings.py.example to settings.py and edit
 that file.
 
 Running the script will create a directory of downloaded stories as a
-subdirectory of CACHEDIR as specified in settings.py, e.g cache/2017.05/ . It
+subdirectory of CACHEDIR as specified in settings.py, e.g cache/2017/12/ . It
 will also download the cover image there.
+
+
+
+# Documentation
+
+If you want to generate documentation, you need to install sphinx and run make:
+
+	$ pip install Sphinx
+	$ cd docs/ && make html
+
+Entry point for the generated documentation is at `docs/_build/html/index.html`
--- jw2epub.py
+++ jw2epub.py
@@ -4,7 +4,7 @@
 
 Download an issue of Jungle World and convert it to epub.
 
-See https://jungle-world.com
+See https://jungle.world
 """
 
 import datetime
@@ -32,9 +32,8 @@ LOGGER = logging.getLogger(__name__)
 class Settings(object):
     """Settings for running jw2epub"""
     CACHEDIR = 'cache'
-    SERVER = 'https://jungle-world.com'
-    URI_INDEX = '/inhalt/'
-    URI_ARTICLE = '/artikel/'
+    SERVER = 'https://jungle.world'
+    URI_INDEX = '/inhalt'
 
 
 
@@ -46,32 +45,48 @@ class JW2EPUB(object):
         if not os.path.exists(self.settings.CACHEDIR):
             os.mkdir(self.settings.CACHEDIR)
 
-        if issue_no:
-            self.issue_no = issue_no
-            self.uri_index = '{}{}/'.format(
-                self.settings.URI_ARTICLE,
-                issue_no.replace('.', '/'),
-            )
-        else:
-            self.issue_no = 'unknown'
-            self.uri_index = self.settings.URI_INDEX
+        if not issue_no:
+            issue_no = self._find_current_issue_no()
 
+        self.issue_no = issue_no
+        self.uri_index = '{}/{}'.format(self.settings.URI_INDEX, issue_no)
         self.issue_dir = os.path.join(self.settings.CACHEDIR, self.issue_no)
-        self.title = 'Unknown issue of Jungle World'
+        self.title = 'Issue {} of Jungle World'.format(self.issue_no)
         self.uri_cover = ''
 
+        
         # Not sure if this actually works, do not have a working login atm
         if hasattr(self.settings, 'USER') and self.settings.USER and\
             hasattr(self.settings, 'PASSWORD') and self.settings.PASSWORD:
-            passman = request.HTTPPasswordMgrWithDefaultRealm()
-            passman.add_password(
-                None,
-                self.settings.SERVER,
-                self.settings.USER,
-                self.settings.PASSWORD)
-            authhandler = request.HTTPBasicAuthHandler(passman)
-            opener = request.build_opener(authhandler)
-            request.install_opener(opener)
+            self._login()
+
+    def _find_current_issue_no(self):
+        """Finds the current issue number"""
+        try:
+            url = self.settings.SERVER
+            LOGGER.info('Find current issue no from %s ...', url)
+            html = request.urlopen(url).read().decode()
+            soup = BeautifulSoup(html, 'html.parser')
+            issue_no = soup.find(attrs={'class': 'view-mode-teaser'}).\
+                find('time').\
+                text
+            return issue_no
+        except client.BadStatusLine as err:
+            LOGGER.warning('Failed fetching: %s', err)
+            return None
+
+
+    def _login(self):
+        """Login to the JW server"""
+        passman = request.HTTPPasswordMgrWithDefaultRealm()
+        passman.add_password(
+            None,
+            self.settings.SERVER,
+            self.settings.USER,
+            self.settings.PASSWORD)
+        authhandler = request.HTTPBasicAuthHandler(passman)
+        opener = request.build_opener(authhandler)
+        request.install_opener(opener)
 
 
     def _fetch_html_file(self, filename):
@@ -98,7 +113,8 @@ class JW2EPUB(object):
         """
         LOGGER.info('Fetch from url %s ...', url)
         try:
-            return request.urlopen(url).read().decode()
+            html = request.urlopen(url).read().decode()
+            return html
         except client.BadStatusLine as err:
             LOGGER.warning('Failed fetching: %s', err)
             return None
@@ -144,7 +160,7 @@ class JW2EPUB(object):
             filename = os.path.join(cachedir, 'index.html')
         else:
             basename = os.path.basename(uri)
-            filename = os.path.join(self.issue_dir, basename)
+            filename = os.path.join(self.issue_dir, '{}.html'.format(basename))
 
         # always fetch index from url
         if not is_index and os.path.exists(filename):
@@ -175,14 +191,16 @@ class JW2EPUB(object):
         index = self._fetch_html(self.uri_index, True)
         soup = BeautifulSoup(index, 'html.parser')
 
-        div = soup.find('div', attrs={'class':'cover_thumb'})
-        self.title = div.find('p').text
+        self.uri_cover = soup.\
+            find('div', attrs={'class':'field-name-field-ref-bilder'}).\
+            find('img').\
+            attrs['src'].\
+            split('?')[0]  # remove token
+        self.title = soup.find('title').text.split('-')[1].strip()
+        self.issue_no = soup.find('ul', attrs={'class': 'breadcrumb'}).\
+            find('time').\
+            text
 
-        # e.g. title == 'Jungle World Nr. 31/12,2. August 2012'
-        issue = self.title.split('.')[1:2][0].split(',')[0].strip().split('/')
-        # issue is not prefixed with century
-        century = str(datetime.datetime.now().year)[:2]
-        self.issue_no = century + issue[1] + '.' + issue[0]
         self.issue_dir = os.path.join(cachedir, self.issue_no)
         if not os.path.exists(self.issue_dir):
             os.makedirs(self.issue_dir)
@@ -191,15 +209,9 @@ class JW2EPUB(object):
             os.path.join(self.issue_dir, 'index.html')
         )
 
-        img_src = dict(div.find('img').attrs)['src']
-        self.uri_cover = img_src.replace('thumb_', '')
-
-        LOGGER.info(
-            'META info: title %s, issue_no %s uri_cover %s',
-            self.title,
-            self.issue_no,
-            self.uri_cover
-        )
+        LOGGER.info('META Title: %s', self.title)
+        LOGGER.info('META Issue No: %s', self.issue_no)
+        LOGGER.info('META URI Cover: %s', self.uri_cover)
         return soup
 
 
@@ -211,43 +223,45 @@ class JW2EPUB(object):
         :return: story
         :rtype: Dict(str, str)
         """
-        # possibly links to other issues
-        if not uri.endswith('.html'):
-            return None
-        if self.issue_no.replace('.', '/') not in uri:
-            return None
-
         html = self._fetch_html(uri)
         if not html:
             return None
 
+        margin = 'margin-top: 0.5em;'
+        #margin = ''
         soup = BeautifulSoup(html, 'html.parser')
-        story = soup.find('div', attrs={'class':'story'})
+        story = soup.find(attrs={'class':'view-mode-full'})
 
-        try: # add class chapter for e.g. automatic calibre TOC generation
-            story.find('h1').attrs.append(('class', 'chapter'))
-        except AttributeError:
-            pass
+        date = story.find(attrs={'id': 'ausgabe-wrapper'}).find('span')
+        if date:
+            date = str(date).replace('span', 'div')
 
-        try: # remove print button
-            story.find('div', {'class': 'menuR'}).extract()
-        except AttributeError:
-            pass
+        title = story.find(attrs={'class': 'page-title'})
+        if title:
+            # add class chapter for e.g. automatic calibre TOC generation
+            title.attrs['class'].append('chapter')
+            title.attrs['style'] = margin
 
-        try: # remove share buttons
-            story.find('p', {'class': 'share'}).extract()
-        except AttributeError:
-            pass
+        lead = story.find(attrs={'class': 'lead'})
+        if lead:
+            lead.attrs['style'] = '{}{}'.format(margin, 'font-weight: bold;')
 
-        if story:
-            output = {
-                'uri': uri,
-                'title': story.find('h1').find('span').text,
-                'html': '<html><body>{}</body></html'.format(str(story))
-            }
-            return output
-        else:
-            return None
+        author = story.find(attrs={'class': 'autor-wrapper'})
+        if author:
+            author.attrs['style'] = margin
+
+        body = story.find(attrs={'class': 'field-name-body'})
+        if body:
+            body.attrs['style'] = margin
+
+        html =' <html><body>{}{}{}{}{}</body></html'.format(
+            date, title, lead, author, body)
+        output = {
+            'uri': uri,
+            'title': title.text,
+            'html': html,
+        }
+        return output
 
 
     def get_stories(self, soup):
@@ -258,19 +272,13 @@ class JW2EPUB(object):
         :return: all stories of current issue.
         :rtype: List(Dict(str, str))
         """
-        regex = re.compile('/artikel')
-        first = True
         stories = []
         parsed = []
 
         LOGGER.info('Get stories ...')
-        for anchor in soup.findAll('a', attrs={'href':regex}):
-            if first:
-                first = False
-                continue
-
+        for story in soup.findAll('h4', attrs={'class': 'public'}):
             try:
-                uri = dict(anchor.attrs)['href']
+                uri = story.find('a').attrs['href']
             except IndexError:
                 continue
 
@@ -323,7 +331,7 @@ class JW2EPUB(object):
 
         for story in stories:
             LOGGER.info('Add story %s ...', story['title'])
-            page = os.path.basename(story['uri'])
+            page = '{}.html'.format(os.path.basename(story['uri']))
             book.files[page] = File(story['html'].encode('utf-8'))
             book.spine.append(Joint(page))
             book.toc.append(Section(story['title'], page))
@@ -336,7 +344,7 @@ class JW2EPUB(object):
         index = self.parse_index()
         stories = self.get_stories(index)
         book = self.make_book(stories)
-        filename_book = 'JW-{}.epub'.format(self.issue_no)
+        filename_book = 'JW-{}.epub'.format(self.issue_no.replace('/', '.'))
         LOGGER.info('Write to %s', filename_book)
         book.write(filename_book)
 
